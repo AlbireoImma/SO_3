2761e389db5915c0628996d91170198c499a90d5
AlbireoImma <faam1612@gmail.com>
2018-11-28 22:30:46 -0300

    [WIP] Creacion clase Venta

--- Venta.java
+++ Venta.java
@@ -1,6 +1,31 @@
 public class Venta {
-    private int turno;
-    private String cliente;
-    private String producto;
-    private 
+    private int turno=0;
+    private int valor=0;
+    private String cliente="";
+    private String producto="";
+    private String fecha="";
+    
+    public String GetFecha(){
+        return new SimpleDateFormat("HH:mm:ss dd/MM/yyyy").format(Calendar.getInstance().getTime());
+    }
+    public void SetValor(int A){
+        synchronized(this){
+            valor = A;
+        }
+    }
+    public void SetCliente(String A){
+        synchronized(this){
+            cliente = A;
+        }
+    }
+    public void SetFecha(){
+        synchronized(this){
+            fecha = GetFecha();
+        }
+    }
+    public void SetProducto(String A){
+        producto = A;
+    }
 }
+
+//String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
