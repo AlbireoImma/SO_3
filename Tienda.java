bcee08748f5b3cbcc9352af5c473a627907bf8a2
AlbireoImma <faam1612@gmail.com>
2018-11-29 00:35:31 -0300

    Implementacion Scanner, Comienzo del Desarrollo del Cliente

--- Tienda.java
+++ Tienda.java
@@ -1,12 +1,23 @@
+import java.util.Scanner;
+
 public class Tienda {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         Venta venta = new Venta();
-        Promotor promotor = new Promotor();
-        Caja caja = new Caja();
-        Empaque empaque = new Empaque();
+        Promotor promotor = new Promotor(venta,reader);
+        Caja caja = new Caja(venta,reader);
+        Empaque empaque = new Empaque(venta,reader);
         promotor.start();
         caja.start();
         empaque.start();
-        
+        while (true) {
+            if (venta.GetTurno()==0) {
+                System.out.println("[Cliente] Entra a la tienda");
+                System.out.println("Ingrese su nombre: ");
+                String nombre = reader.next();
+                venta.SetCliente(nombre);
+                venta.AddTurno();
+            }
+        }
     }
 }
