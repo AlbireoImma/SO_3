cc87c406f605f55bba0364288e497a3751a6f9cd
AlbireoImma <francisco@ubuntu.ubuntu-domain>
2018-11-29 12:30:32 -0300

    [WIP] Implementacion run()

--- Empaque.java
+++ Empaque.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
 
-public class Empaque extends Thread{
-    private int empacados=0;
+public class Empaque extends Thread {
+    private int empacados = 0;
     private Venta venta = null;
     Scanner reader = null;
-    Empaque(Venta venta,Scanner reader){
+
+    Empaque(Venta venta, Scanner reader) {
         this.venta = venta;
         this.reader = reader;
     }
-    public void run(){
+
+    public void run() {
         Thread.currentThread().setName("Empaque");
-        System.out.println("Tienda en marcha");
+        while (true) {
+            if (venta.GetTurno() == 3) {
+                System.out.println("[" + venta.GetFecha() + "][Empaque] Paso por Empaque");
+                venta.AddTurno();
+            }
+        }
     }
 }
