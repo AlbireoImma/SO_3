bd51d65ce8739c702d5edcbaf1d310f472baa270
AlbireoImma <francisco@ubuntu.ubuntu-domain>
2018-11-29 13:15:25 -0300

    Beautify

--- Venta.java
+++ Venta.java
@@ -2,85 +2,97 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Venta {
-    private int turno=0;
-    private int valor=0;
-    private String cliente="";
-    private String producto="";
-    private String fecha="";
+    private int turno = 0;
+    private int valor = 0;
+    private String cliente = "";
+    private String producto = "";
+    private String fecha = "";
     private Productos productos = new Productos();
-    
-    public String GetFecha(){
+
+    public String GetFecha() {
         return new SimpleDateFormat("HH:mm:ss dd/MM/yyyy").format(Calendar.getInstance().getTime());
     }
-    public void SetValor(int A){
-        synchronized(this){
+
+    public void SetValor(int A) {
+        synchronized (this) {
             valor = A;
         }
     }
-    public void SetCliente(String A){
-        synchronized(this){
+
+    public void SetCliente(String A) {
+        synchronized (this) {
             cliente = A;
         }
     }
-    public void SetFecha(){
-        synchronized(this){
+
+    public void SetFecha() {
+        synchronized (this) {
             fecha = GetFecha();
         }
     }
-    public void SetProducto(String A){
-        synchronized(this){
+
+    public void SetProducto(String A) {
+        synchronized (this) {
             producto = A;
         }
     }
-    public void AddTurno(){
-        synchronized(this){
+
+    public void AddTurno() {
+        synchronized (this) {
             turno++;
             turno %= 4;
         }
     }
-    public void SetTurno(int A){
-        synchronized(this){
+
+    public void SetTurno(int A) {
+        synchronized (this) {
             turno = A;
         }
     }
-    public int GetTurno(){
-        synchronized(this){
+
+    public int GetTurno() {
+        synchronized (this) {
             return turno;
         }
     }
-    public int GetValor(){
-        synchronized(this){
+
+    public int GetValor() {
+        synchronized (this) {
             return valor;
         }
     }
-    public String GetCliente(){
-        synchronized(this){
+
+    public String GetCliente() {
+        synchronized (this) {
             return cliente;
         }
     }
-    public String GetProducto(){
-        synchronized(this){
+
+    public String GetProducto() {
+        synchronized (this) {
             return producto;
         }
     }
-    public String GetFecha_(){
-        synchronized(this){
+
+    public String GetFecha_() {
+        synchronized (this) {
             return fecha;
         }
     }
-    public void PrintProductos(){
+
+    public void PrintProductos() {
         productos.PrintProductos();
     }
-    public String GetNombreProductos(int index){
-        synchronized(this){
+
+    public String GetNombreProductos(int index) {
+        synchronized (this) {
             return productos.GetNombre(index);
         }
     }
-    public int GetPrecioProductos(int index){
-        synchronized(this){
+
+    public int GetPrecioProductos(int index) {
+        synchronized (this) {
             return productos.GetPrecio(index);
         }
     }
 }
-
-//String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
