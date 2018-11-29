2da93ba27606267a047136c66d1a975d9f1783d8
AlbireoImma <faam1612@gmail.com>
2018-11-28 23:26:28 -0300

    Instaciancion de venta

--- Tienda.java
+++ Tienda.java
@@ -1,10 +1,12 @@
 public class Tienda {
     public static void main(String[] args) {
+        Venta venta = new Venta();
         Promotor promotor = new Promotor();
         Caja caja = new Caja();
         Empaque empaque = new Empaque();
         promotor.start();
         caja.start();
         empaque.start();
+        
     }
 }
