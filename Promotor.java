51dfc3f7ddbe633dbb59e881102bdc2f3a97c0a2
AlbireoImma <francisco@ubuntu.ubuntu-domain>
2018-11-29 12:31:16 -0300

    [WIP] Implementacion run()

--- Promotor.java
+++ Promotor.java
@@ -1,43 +1,53 @@
 import java.util.Scanner;
-public class Promotor extends Thread{
+
+public class Promotor extends Thread {
     private int atendidos;
     private Venta venta = null;
     Scanner reader = null;
-    Promotor(Venta venta, Scanner reader){
+
+    Promotor(Venta venta, Scanner reader) {
         this.venta = venta;
         this.reader = reader;
         this.atendidos = 0;
     }
-    public void run(){
+
+    public void run() {
         Thread.currentThread().setName("Promotor");
         while (true) {
-            if (venta.GetTurno()==1) {
-                System.out.println("[Promotor] '"+venta.GetCliente()+", tengo los siguientes productos para ofrecerte'");
+            if (venta.GetTurno() == 1) {
+                System.out.println("[" + venta.GetFecha() + "][Promotor] '" + venta.GetCliente()
+                        + ", tengo los siguientes productos para ofrecerte'");
                 venta.PrintProductos();
                 System.out.println("Ingrese una opcion [Use cualquier otra para cancelar]: ");
                 String seleccion = reader.next();
                 int n = Integer.parseInt(seleccion);
                 if (n == 1) {
-                    venta.SetProducto(venta.GetNombreProductos(n-1));
-                    venta.SetValor(venta.GetPrecioProductos(n-1));
+                    venta.SetProducto(venta.GetNombreProductos(n - 1));
+                    venta.SetValor(venta.GetPrecioProductos(n - 1));
                     atendidos++;
+                    System.out.println("[" + venta.GetFecha() + "][Promotor] Atendio a " + venta.GetCliente()
+                            + ", mediante la compra de " + venta.GetProducto());
                     venta.AddTurno();
                 } else if (n == 2) {
-                    venta.SetProducto(venta.GetNombreProductos(n-1));
-                    venta.SetValor(venta.GetPrecioProductos(n-1));
+                    venta.SetProducto(venta.GetNombreProductos(n - 1));
+                    venta.SetValor(venta.GetPrecioProductos(n - 1));
                     atendidos++;
+                    System.out.println("[" + venta.GetFecha() + "][Promotor] Atendio a " + venta.GetCliente()
+                            + ", mediante la compra de " + venta.GetProducto());
                     venta.AddTurno();
                 } else if (n == 3) {
-                    venta.SetProducto(venta.GetNombreProductos(n-1));
-                    venta.SetValor(venta.GetPrecioProductos(n-1));
+                    venta.SetProducto(venta.GetNombreProductos(n - 1));
+                    venta.SetValor(venta.GetPrecioProductos(n - 1));
                     atendidos++;
+                    System.out.println("[" + venta.GetFecha() + "][Promotor] Atendio a " + venta.GetCliente()
+                            + ", mediante la compra de " + venta.GetProducto());
                     venta.AddTurno();
-                } else {
                     atendidos++;
+                } else {
+                    System.out.println("[" + venta.GetFecha() + "][Promotor] Atendio a " + venta.GetCliente()
+                            + " sin compra por parte del cliente");
                     venta.SetTurno(0);
                 }
-                reader.close();
-                
             }
         }
     }
--- Tienda.java
+++ Tienda.java
@@ -4,16 +4,16 @@ public class Tienda {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         Venta venta = new Venta();
-        Promotor promotor = new Promotor(venta,reader);
-        Caja caja = new Caja(venta,reader);
-        Empaque empaque = new Empaque(venta,reader);
+        Promotor promotor = new Promotor(venta, reader);
+        Caja caja = new Caja(venta, reader);
+        Empaque empaque = new Empaque(venta, reader);
         promotor.start();
         caja.start();
         empaque.start();
         while (true) {
-            if (venta.GetTurno()==0) {
-                System.out.println("[Cliente] Entra a la tienda");
-                System.out.println("Ingrese su nombre: ");
+            if (venta.GetTurno() == 0) {
+                System.out.println("[" + venta.GetFecha() + "][Cliente] Entra a la tienda");
+                System.out.println("[Cliente] Ingrese su nombre: ");
                 String nombre = reader.next();
                 venta.SetCliente(nombre);
                 venta.AddTurno();
