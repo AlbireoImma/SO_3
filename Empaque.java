2bfbb713b2d4b1f0c52ea6cac3b3b7f6d1a484aa
AlbireoImma <faam1612@gmail.com>
2018-12-01 21:44:05 -0300

    [WDN] Creacion del log

--- Empaque.java
+++ Empaque.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Empaque extends Thread {
     private int empacados = 0;
     private Venta venta = null;
-    Scanner reader = null;
+    private Scanner reader = null;
+    private PrintStream out = null;
 
     Empaque(Venta venta, Scanner reader) {
         this.venta = venta;
@@ -14,9 +15,19 @@ public class Empaque extends Thread {
         Thread.currentThread().setName("Empaque");
         while (true) {
             if (venta.GetTurno() == 3) {
-                System.out.println("[" + venta.GetFecha() + "][Empaque] Empaque de " + venta.GetProducto());
                 empacados++;
-                System.out.println("[" + venta.GetFecha() + "][Empaque] Productos empacados: " + empacados);
+                String fase_1 = "[" + venta.GetFecha() + "][Empaque] Empaque de " + venta.GetProducto();
+                String fase_2 = "[" + venta.GetFecha() + "][Empaque] Productos empacados: " + empacados;
+                System.out.println(fase_1);
+                System.out.println(fase_2);
+                try {
+                    out = new PrintStream(new FileOutputStream("log_Empaque.txt", true));
+                } catch (Exception e) {
+                    // TODO: handle exception
+                } finally {
+                    out.println(fase_1);
+                    out.println(fase_2);
+                }
                 venta.AddTurno();
             }
         }
